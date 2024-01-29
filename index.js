9ae5978cadc391ef675fb7aa9910d30ca2e74116
Clinton Selke <clinuxrulz@gmail.com>
2024-01-29 14:08:36 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+import { WebSocketServer } from 'ws';
+
+import { v4 as uuidv4 } from "uuid";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const peers = [];
+
+wss.on('connection', function(ws) {
+    let id = uuidv4();
+
+    ws.on('message', function(message) {
+        let data = message.data;
+        if (typeof data != "string") {
+            return;
+        }
+        let data2;
+        try {
+            data2 = JSON.parse(data);
+        } catch (_ex) {
+        }
+        if (data2 == "KeepAlive") {
+            // do nothing, message recieved to keep connection open.
+        } else if (typeof data2 == "object") {
+            if (data2.hasOwnProperty("Signal")) {
+                let data3 = data2["Signal"];
+                if (data3.hasOwnProperty("receiver") && data3.hasOwnProperty("data")) {
+                    let receiver = data3["receiver"];
+                    let data = data3["data"];
+                    if (typeof receiver == "string") {
+                        let signalMessage = JSON.stringify({
+                            "Signal": {
+                                sender: receiver,
+                                data,
+                            }
+                        });
+                        for (let peer of peers) {
+                            peer.ws.send(signalMessage);
+                        }
+                    }
+                }
+            }
+        }
+    });
+    ws.on("close", function() {
+        for (let i = 0; i < peers.length; ++i) {
+            if (peers[i].id == id) {
+                peers.splice(i, 1);
+                break;
+            }
+        }
+        let peerLeftMessage = JSON.stringify({ "PeerLeft": id });
+        for (let peer of peers) {
+            peer.ws.send(peerLeftMessage);
+        }
+    });
+
+    {
+        let newPeerMessage = JSON.stringify({ "NewPeer": id, });
+        for (let peer of peers) {
+            peer.ws.send(newPeerMessage);
+        }
+    }
+    peers.push({
+        id,
+        ws,
+    });
+    ws.send(JSON.stringify({ "IdAssigned": id, }));
+});
